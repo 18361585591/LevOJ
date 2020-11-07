050acd190be3eccd1c9b7fb5e0115f8b2cbf65ea
Jun Wang <wzx20020111@outlook.com>
2020-11-07 15:18:50 +0800

    Update P1032 平均数.c

--- P1032 平均数.c
+++ P1032 平均数.c
@@ -5,7 +5,7 @@ int main()
 {
 	int m, k, n, i, t, a;
 	double p = 0;
-	int s = 0;
+	int s = 0, q = 0;
 	scanf("%d", &m);
 	for (i = 0; i < m; i++)
 	{
@@ -16,18 +16,23 @@ int main()
 			if (a > n)
 			{
 				s = s + a;
+				q = q + 1;
 			}
 		}
-		p = (double)s / 2;
-		if ((int)p == 0)
+		if (q == 0)
 		{
 			printf("-1\n");
+			p = 0;
+			s = 0;
+			q = 0;
 		}
 		else
 		{
+			p = (double)s / q;
 			printf("%.2f\n", p);
 			p = 0;
 			s = 0;
+			q = 0;
 		}
 	}
 	return 0;
